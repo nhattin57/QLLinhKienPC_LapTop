02457a49793ce7c344367721bdd3b0cfd1cab985
Your Name <you@example.com>
2022-01-07 18:01:58 +0700

    Frm doanh thu xong

--- QLLinhKien/frmThongKe.Designer.cs
+++ QLLinhKien/frmThongKe.Designer.cs
@@ -30,31 +30,110 @@ namespace QLLinhKien
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.lsvHoaDon = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnExcel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(310, 151);
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(828, 369);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(105, 55);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Thống kê";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lsvHoaDon
+            // 
+            this.lsvHoaDon.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.lsvHoaDon.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lsvHoaDon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsvHoaDon.FullRowSelect = true;
+            this.lsvHoaDon.GridLines = true;
+            this.lsvHoaDon.HideSelection = false;
+            this.lsvHoaDon.Location = new System.Drawing.Point(12, 13);
+            this.lsvHoaDon.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.lsvHoaDon.Name = "lsvHoaDon";
+            this.lsvHoaDon.Size = new System.Drawing.Size(1063, 349);
+            this.lsvHoaDon.TabIndex = 1;
+            this.lsvHoaDon.UseCompatibleStateImageBehavior = false;
+            this.lsvHoaDon.View = System.Windows.Forms.View.Details;
+            this.lsvHoaDon.SelectedIndexChanged += new System.EventHandler(this.lsvHoaDon_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã HD";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Khách Hàng";
+            this.columnHeader2.Width = 180;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Nhân Viên";
+            this.columnHeader3.Width = 110;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Ngày Xuất HD";
+            this.columnHeader4.Width = 160;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Tổng Tiền";
+            this.columnHeader5.Width = 160;
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.AutoSize = true;
+            this.btnExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcel.Location = new System.Drawing.Point(961, 369);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(114, 55);
+            this.btnExcel.TabIndex = 2;
+            this.btnExcel.Text = "Xuất excel";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
             // 
             // frmThongKe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1099, 612);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.lsvHoaDon);
             this.Controls.Add(this.button1);
             this.Name = "frmThongKe";
             this.Text = "Thống Kê";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView lsvHoaDon;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnExcel;
     }
 }
--- QLLinhKien/frmThongKe.cs
+++ QLLinhKien/frmThongKe.cs
@@ -16,5 +16,48 @@ namespace QLLinhKien
         {
             InitializeComponent();
         }
+        LinhKien lk = new LinhKien();
+        private void lsvHoaDon_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        void hienThiHoaDon()
+        {
+            DataTable dt = lk.layDSHoaDon();
+            lsvHoaDon.Items.Clear();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                ListViewItem lvi = lsvHoaDon.Items.Add(dt.Rows[i][0].ToString().Trim());
+                lvi.SubItems.Add(dt.Rows[i][1].ToString().Trim());
+                lvi.SubItems.Add(dt.Rows[i][2].ToString().Trim());
+                lvi.SubItems.Add(dt.Rows[i][3].ToString().Trim());
+                lvi.SubItems.Add(dt.Rows[i][4].ToString().Trim());               
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hienThiHoaDon();
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            app.Visible = true;
+            Microsoft.Office.Interop.Excel.Workbook wb = app.Workbooks.Add(1);
+            Microsoft.Office.Interop.Excel.Worksheet ws = (Microsoft.Office.Interop.Excel.Worksheet)wb.Worksheets[1];
+            int i = 1;
+            int i2 = 1;
+            foreach (ListViewItem lvi in lsvHoaDon.Items)
+            {
+                i = 1;
+                foreach (ListViewItem.ListViewSubItem lvs in lvi.SubItems)
+                {
+                    ws.Cells[i2, i] = lvs.Text;
+                    i++;
+                }
+                i2++;
+            }
+        }
     }
 }
