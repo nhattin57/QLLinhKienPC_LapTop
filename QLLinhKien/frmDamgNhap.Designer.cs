641417314b3a3dd0bc9274fe8594f436249d0802
nhattin57 <nhattin57@gmail.com>
2022-01-14 09:33:14 +0700

    laithem

--- QLLinhKien/frmDamgNhap.Designer.cs
+++ QLLinhKien/frmDamgNhap.Designer.cs
@@ -35,7 +35,6 @@ namespace QLLinhKien
             this.label3 = new System.Windows.Forms.Label();
             this.txtMatKhau = new System.Windows.Forms.TextBox();
             this.btnDangNhap = new System.Windows.Forms.Button();
-            this.btnHuy = new System.Windows.Forms.Button();
             this.chkHienThiMK = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
@@ -61,6 +60,7 @@ namespace QLLinhKien
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
             this.label2.Location = new System.Drawing.Point(58, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(442, 32);
@@ -88,25 +88,15 @@ namespace QLLinhKien
             // 
             // btnDangNhap
             // 
+            this.btnDangNhap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.btnDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDangNhap.Location = new System.Drawing.Point(203, 174);
             this.btnDangNhap.Name = "btnDangNhap";
-            this.btnDangNhap.Size = new System.Drawing.Size(135, 42);
+            this.btnDangNhap.Size = new System.Drawing.Size(250, 42);
             this.btnDangNhap.TabIndex = 3;
             this.btnDangNhap.Text = "Đăng Nhập";
-            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.UseVisualStyleBackColor = false;
             this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
-            // 
-            // btnHuy
-            // 
-            this.btnHuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnHuy.Location = new System.Drawing.Point(344, 174);
-            this.btnHuy.Name = "btnHuy";
-            this.btnHuy.Size = new System.Drawing.Size(109, 42);
-            this.btnHuy.TabIndex = 4;
-            this.btnHuy.Text = "Hủy";
-            this.btnHuy.UseVisualStyleBackColor = true;
-            this.btnHuy.Click += new System.EventHandler(this.btnHuy_Click);
             // 
             // chkHienThiMK
             // 
@@ -125,9 +115,9 @@ namespace QLLinhKien
             this.AcceptButton = this.btnDangNhap;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(565, 239);
             this.Controls.Add(this.chkHienThiMK);
-            this.Controls.Add(this.btnHuy);
             this.Controls.Add(this.btnDangNhap);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtMatKhau);
@@ -151,7 +141,6 @@ namespace QLLinhKien
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtMatKhau;
         private System.Windows.Forms.Button btnDangNhap;
-        private System.Windows.Forms.Button btnHuy;
         private System.Windows.Forms.CheckBox chkHienThiMK;
     }
 }
